9fe790384e000748262962c69c974781c2a6e43d
Erick Treichel <erickfili25@gmail.com>
2022-06-25 20:25:26 -0300

    Alteração01

--- src/components/no-search/index.js
+++ src/components/no-search/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 import * as S from "./styled";
+import git1 from "./gitpng1.png";
+import git2 from "./gitpng2.png";
 
+/*
 function NoSearch() {
   return (
     <S.Wrapper>
       <h1>Enter a username and click "Search".</h1>
+      <S.WrapperImage
+        src={"./gitpng1.png"}
+        alt="Github Logo"
+        width="40%"
+        height="40%"
+      />
+    </S.Wrapper>
+  );
+};
+*/
+function NoSearch() {
+  return (
+    <S.Wrapper>
+      <h1>Enter a username and click "Search".</h1>
+      <S.WrapperImage src={git2} />
     </S.Wrapper>
   );
 }
--- src/components/no-search/styled.js
+++ src/components/no-search/styled.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  margin-top: 140px;
+  // margin: 10px 0;
+  margin-top: 180px;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+    align-items: center;
 
   h1 {
     // font-weight: bold;
@@ -12,12 +14,33 @@ export const Wrapper = styled.div`
   }
 
   @media (max-width: 600px) {
-    margin-top: 140px;
+    margin-top: 80px;
     display: flex;
     justify-content: center;
+    align-items: center;
+
     h1 {
       // font-weight: bold;
       font-size: 20px;
+    }
+`;
+export const WrapperImage = styled.img`
+  width: 480px;
+  margin: 0 6px;
+  margin-top: 5%;
+  cursor: pointer;
+  justify-content: center;
+  justify-items: center;
+  justify-self: center;
 
+  &:hover {
+    opacity: 0.7;
+    cursor: default;
+  }
+
+  @media (max-width: 600px) {
+    width: 320px;
+
+    margin-top: 60px;
   }
 `;
